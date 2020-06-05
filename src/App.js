675810038a126a779b7e5da949ba2813f309e3f4
Le Nho Thang <gianggiang1511@gmail.com>
2020-06-05 11:50:48 +0700

    Done Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Products from './components/Products';
+import Message from './components/Message';
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <main id="mainContainer">
+                    <div className="container">
+                        <Products />
+                        <Message />
+                        <Cart />
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
+
