e907b5890e964073608df397bb9d7ea99522a5b9
Le Nho Thang <gianggiang1511@gmail.com>
2020-06-16 14:33:27 +0700

    Done Show product with react-redux

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,27 +1,30 @@
 var initialState = [
     {
         id: 1,
-        name: 'Iphone 7 Plus',
-        image: '',
+        name: 'Iphone X',
+        image: 'https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg',
         des: 'Sản phẩm do Apple sản xuất',
         price: 500,
-        inventory: 10
+        inventory: 10,
+        rating: 4
     },
     {
         id: 2,
-        name: 'Samsung Galaxy S7',
-        image: '',
+        name: 'Samsung Galaxy Fold',
+        image: 'https://cdn.tgdd.vn/Products/Images/42/198158/samsung-galaxy-fold-black-600x600.jpg',
         des: 'Sản phẩm do samsung sản xuất',
         price: 400,
-        inventory: 15
+        inventory: 15,
+        rating: 5
     },
     {
         id: 3,
-        name: 'Oppo F1s',
-        image: '',
+        name: 'Oppo R17 Pro',
+        image: 'https://halomobile.vn/wp-content/uploads/2018/03/oppo-r17-pro-600x600.jpg',
         des: 'Sản phẩm do china sản xuất',
         price: 450,
-        inventory: 5
+        inventory: 5,
+        rating: 5
     },
 ];
 
