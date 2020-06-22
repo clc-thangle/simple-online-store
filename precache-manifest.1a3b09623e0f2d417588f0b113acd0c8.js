cb1a1c5e70e6cbbf77be0301a408be3e14e29642
Le Nho Thang <gianggiang1511@gmail.com>
2020-06-22 21:05:21 +0700

    Updates

---
+++ precache-manifest.1a3b09623e0f2d417588f0b113acd0c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964a412cd958166b8fac904b60f85cae",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "7115e214b5f606d37983",
+    "url": "/deploy-github/static/js/2.bb72719d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/deploy-github/static/js/2.bb72719d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe22618dd093d22c227",
+    "url": "/deploy-github/static/js/main.5c8dda8b.chunk.js"
+  },
+  {
+    "revision": "d337f2a26a7f37c0de15",
+    "url": "/deploy-github/static/js/runtime-main.4c01bb90.js"
+  }
+]);
